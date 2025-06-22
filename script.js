ea183a352d25ea24aecf8246b9ed30d770ad8a6a
06091210 <firp771@gmail.com>
2025-06-22 14:16:32 +0900

    Update script.js

--- script.js
+++ script.js
@@ -129,7 +129,7 @@ function showResult() {
   const percentage = (score / currentQuiz.length) * 100;
   document.getElementById("score").textContent = `正解数: ${score}/${
     currentQuiz.length
-  } (${percentage.toFixed(7)}%)`;
+  } (${percentage.toFixed(quizData.length)}%)`;
 }
 function restartQuiz() {
   currentQuestion = 0;
